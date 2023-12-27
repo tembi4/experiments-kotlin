df3dbb5fd0081bc0686e056a382d8b34409d03bb
Artem Popenkov <Artem.Popenkov@sap.com>
2023-12-27 18:12:26 +0100

    refactoring

--- src/main/kotlin/adventofcode/day14/Puzzle.kt
+++ src/main/kotlin/adventofcode/day14/Puzzle.kt
@@ -15,19 +15,18 @@ class Puzzle {
 
             val queue = initialPolymerTemplate.windowed(2).toMutableList()
 
-            for (step in 1 .. totalSteps) {
+            for (step in 1..totalSteps) {
                 println("Current queue size is ${queue.size}")
 
                 val newTasks = mutableListOf<String>()
                 queue.forEach { pair ->
-                        // Each insertion will increase the Count
-                        val symbolToInsert = pairInsertions[pair]
-                        var currentCount = symbolsCount.getOrDefault(symbolToInsert!!, 0)
-                        symbolsCount[symbolToInsert] = currentCount + 1
+                    // Each insertion will increase the Count
+                    val symbolToInsert = pairInsertions[pair]
+                    symbolsCount.increase(key = symbolToInsert!!, increment = 1)
 
-                        // Create 2 more tasks
-                        newTasks.add("${pair[0]}$symbolToInsert")
-                        newTasks.add("$symbolToInsert${pair[1]}")
+                    // Create 2 more tasks
+                    newTasks.add("${pair[0]}$symbolToInsert")
+                    newTasks.add("$symbolToInsert${pair[1]}")
                 }
                 queue.clear()
                 queue.addAll(newTasks)
@@ -38,6 +37,11 @@ class Puzzle {
             val max = symbolsCount.maxOf { it.value }
 
             return max - min
+        }
+
+        private fun MutableMap<Char, Int>.increase(key: Char, increment: Int) {
+            var currentCount = this.getOrDefault(key, 0)
+            this[key] = currentCount + increment
         }
     }
 }
